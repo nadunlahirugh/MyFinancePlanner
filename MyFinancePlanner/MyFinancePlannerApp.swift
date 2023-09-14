53a281464f819d3aa1ead97361be0e181d7403e3
nadunlahirugh <84806244+nadunlahirugh@users.noreply.github.com>
2023-09-14 12:35:10 +0530

    Add files via upload

---
+++ MyFinancePlanner/MyFinancePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinancePlannerApp.swift
+//  MyFinancePlanner
+//
+//  Created by Akvasoft Pvt Ltd on 2023-09-14.
+//
+
+import SwiftUI
+
+@main
+struct MyFinancePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
